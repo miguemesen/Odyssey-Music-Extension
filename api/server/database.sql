ace341b2f03b4b64f1dc729546f185097a757f72
miguemesen <miguemesen12@gmail.com>
2021-01-25 18:41:45 -0600

    Fixed database

--- api/server/database.sql
+++ api/server/database.sql
@@ -1,72 +1,38 @@
 CREATE DATABASE IF NOT EXISTS MUSIC_DATABASE;
-
 USE MUSIC_DATABASE;
-SET GLOBAL log_bin_trust_function_creators = 1;
 
-
-CREATE TABLE IF NOT EXISTS users (
-
-    id INTEGER NOT NULL
+CREATE TABLE IF NOT EXISTS users(
+    id LONG NOT NULL,
+    email CHAR(128) NOT NULL
 );
-
-
-CREATE TABLE IF NOT EXISTS songs (
-
-    id INTEGER NOT NULL,
-    song_name char(32) NOT NULL,
-    artist char(32) NOT NULL,
-    album char(32)
+CREATE TABLE IF NOT EXISTS songs(
+    Track_Name char(128) NOT NULL,
+    Album_Name char(128) NOT NULL,
+    Artist_Name char(128) NOT NULL
 
 );
-
-
-CREATE FUNCTION add_user(new_id INTEGER)
-    RETURNS BOOL
-    BEGIN
-
-        DECLARE ID INTEGER;
-        SELECT id INTO ID FROM users WHERE id=new_id;
-
-        IF (ID IS NULL)
-            THEN INSERT INTO users (id) VALUES (new_id);
-
-        END IF;
-
-        RETURN 1;
-    END;
-
 
 CREATE PROCEDURE get_users()
     BEGIN
         SELECT  * FROM users;
     END;
 
-
-CREATE PROCEDURE add_user(new_id INTEGER)
+CREATE PROCEDURE add_user(new_id INTEGER, new_email char(64))
     BEGIN
-        INSERT INTO users (id) VALUES (new_id);
+        INSERT INTO users (id,email) VALUES (new_id,new_email);
     END;
-
-
 CREATE PROCEDURE delete_user(id_ INTEGER)
     BEGIN
-
         DELETE FROM users WHERE id=id_;
-
     END;
-
-
-CREATE PROCEDURE add_song(id_ INTEGER, song_name_ CHAR(32), artist_ CHAR(32), album_ CHAR(32))
-    BEGIN
-
-        INSERT INTO songs (id, song_name, artist, album) VALUES (id_, song_name_, artist_, album_);
-    END;
-
-
 CREATE PROCEDURE get_songs()
     BEGIN
-
         SELECT * FROM songs;
-
+    END;
+CREATE PROCEDURE get_song(song_stuff char(128))
+    BEGIN
+        SELECT *
+        FROM songs
+        WHERE Track_Name REGEXP song_stuff OR Artist_Name REGEXP song_stuff OR Album_Name REGEXP song_stuff;
     END;
 
